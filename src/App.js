1ad4a667de629f076fd559e444255831bc8d0f5f
Greta Va <greta.vasilevskyte@gmail.com>
2023-05-09 17:18:14 +0200

    Update project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className={styles.head}>my header</div>
       </header>
-    </div>
+      <main>my content</main>
+      <footer>footer</footer>
+    </>
   );
 }
